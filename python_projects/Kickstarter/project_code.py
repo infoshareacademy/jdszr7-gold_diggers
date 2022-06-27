b7acfed0cfd8a95f0e83e2e86c77aff419ce0019
Ela Markiewicz <markiewcz.elzbieta@gmail.com>
2022-06-27 18:22:56 +0200

    project_code.py update

--- python_projects/Kickstarter/project_code.py
+++ python_projects/Kickstarter/project_code.py
@@ -1,17 +1,29 @@
 import pandas as pd
 import matplotlib as plt
+import seaborn as sns
 
+#funkcja import pliku
 def f_file_import(name:str):
     return pd.read_csv(name)
 
-#funkcja wykres liczebność jedna zmienna
-def f_barplot_1_var(name:str, var:str):
-    return name.groupby(var)[var].count().sort_values(ascending = False).plot(kind = 'bar', figsize = (10,6));
+
+#funkcja agregująca dane do nowego DF
+def aggregate_fun(df, agg_list:list, measure:str):
+    agg_df = df.groupby(agg_list)[measure].agg(['count',sum,'mean']).reset_index()
+    return agg_df
 
 
-def f_barplot_2_var_count(name:str, var1:str, var2:str):
-    return sns.catplot(data = name, col = var1, x = var2, y = 'count', kind = 'bar');
 
+#funkcja wykres słupkowy - liczebność - jedna zmienna
+def f_barplot_1_var(name, var:str):
+    name.groupby(var)[var].count().sort_values(ascending = False).plot(kind = 'bar', figsize = (10,6));
+
+#funkcja wykres słupkowy - liczebność - dwie zmienne - col x
+def barplot_2var(df, var1:str, var2:str, measure):
+    sns.catplot(data = df, col = var1 , x = var2, y = measure , kind = 'bar');
+
+#funkcja wykres słupkowy - średnia - dwie zmienne
 # var1 - zmienna grupująca, var2 - zmienna grupowana
-def f_barplot_2_var_mean(name:str, var1:str, var2:str):
-    return df_clean.groupby(var1)[var2].mean().sort_values(ascending=False).plot(kind='bar', figsize=(10,8));
+def f_barplot_2_var_mean(name, var1:str, var2:str):
+    name.groupby(var1)[var2].mean().sort_values(ascending=False).plot(kind='bar', figsize=(10,8));
+
