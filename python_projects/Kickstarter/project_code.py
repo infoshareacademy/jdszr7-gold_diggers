1114607fbd39ef808bd1cb980df0aa488207280d
Ela Markiewicz <markiewcz.elzbieta@gmail.com>
2022-06-27 20:31:38 +0200

    ela development 20220627

--- python_projects/Kickstarter/project_code.py
+++ python_projects/Kickstarter/project_code.py
@@ -12,11 +12,15 @@ def aggregate_fun(df, agg_list:list, measure:str):
     agg_df = df.groupby(agg_list)[measure].agg(['count',sum,'mean']).reset_index()
     return agg_df
 
+#funkcja sortująca
+def sorting_values(df_agg, value_by = 'mean', sorting_var = False):
+    return df_agg.sort_values(by =value_by, ascending= sorting_var).reset_index()
+
 
 
 #funkcja wykres słupkowy - liczebność - jedna zmienna
-def f_barplot_1_var(name, var:str):
-    name.groupby(var)[var].count().sort_values(ascending = False).plot(kind = 'bar', figsize = (10,6));
+def f_barplot_1_var(name, var:str, size:tuple = (10,6)):
+    name.groupby(var)[var].count().sort_values(ascending = False).plot(kind = 'bar', figsize = size);
 
 #funkcja wykres słupkowy - liczebność - dwie zmienne - col x
 def barplot_2var(df, var1:str, var2:str, measure):
@@ -24,6 +28,6 @@ def barplot_2var(df, var1:str, var2:str, measure):
 
 #funkcja wykres słupkowy - średnia - dwie zmienne
 # var1 - zmienna grupująca, var2 - zmienna grupowana
-def f_barplot_2_var_mean(name, var1:str, var2:str):
-    name.groupby(var1)[var2].mean().sort_values(ascending=False).plot(kind='bar', figsize=(10,8));
+def f_barplot_2_var_mean(name, var1:str, var2:str, size:tuple = (10,6)):
+    name.groupby(var1)[var2].mean().sort_values(ascending=False).plot(kind='bar', figsize=size);
 
